8470723ccc2c164a965c0fbb5d6846e2c8d95224
floriankaiser <floriankaiser@florians-mbp>
2016-05-02 10:42:29 +0200

    änderung Polymorhie

--- src/Polymorphie/Abteilungsleiter.java
+++ src/Polymorphie/Abteilungsleiter.java
@@ -3,20 +3,19 @@ package Polymorphie;
 public class Abteilungsleiter extends Angestellter {
 
 	public String abteilung;
-	
+
 	public Abteilungsleiter(String abteilung) {
 		this.abteilung = abteilung;
 		// TODO Auto-generated constructor stub
 	}
 
 	public String lieferePersonalien() {
-		return getName() + " " + this.gehalt + " " +  this.geburtsdatum +  " " + this.abteilung;
+		return getName() + " " + this.gehalt + " " + this.geburtsdatum + " "
+				+ this.abteilung;
 	}
 
 	public void befördern(Angestellter a) {
-       
-    }
+
+	}
 
 }
-
-
